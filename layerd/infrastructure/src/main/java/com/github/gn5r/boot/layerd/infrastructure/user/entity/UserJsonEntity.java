5809af57375cb411b4c5ac377b66f7d124454f7d
gn5r <via.gn5r@hotmail.com>
2025-06-04 18:08:46 +0000

    feat: Implement user retrieval functionality
    - Created GetUserUseCase interface for user retrieval by ID.
    - Added UserDto class to represent user data transfer object.
    - Implemented GetUserUseCaseImpl to handle user retrieval logic.
    - Introduced UserEntity class to represent user in the domain layer.
    - Created UserRepository interface for user data access.
    - Implemented UserRepositoryImpl to fetch user data from JSON file.
    - Added UserJsonEntity class to map user data from JSON.
    - Created UserJsonRepository interface and its implementation for JSON data access.
    - Added users.json file to provide sample user data.
    - Set up Maven wrapper scripts for easier project setup.
    - Established multi-module Maven project structure with application, domain, and infrastructure modules.
    - Developed UserRestController to expose user retrieval API endpoint.
    - Configured application properties for Spring Boot application.
    - Added unit tests for LayerdApplication to ensure context loads correctly.

---
+++ layerd/infrastructure/src/main/java/com/github/gn5r/boot/layerd/infrastructure/user/entity/UserJsonEntity.java
@@ -0,0 +1,12 @@
+package com.github.gn5r.boot.layerd.infrastructure.user.entity;
+
+import lombok.Data;
+
+@Data
+public class UserJsonEntity {
+
+  private Integer id;
+  private String name;
+  private String email;
+
+}
---
+++ layerd/presentation/src/test/java/com/github/gn5r/boot/layerd/LayerdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gn5r.boot.layerd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayerdApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
